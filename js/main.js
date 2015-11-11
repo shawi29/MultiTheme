03ddba4f0d1be0d15a6d7270cde571bdc5258287
shawi29 <sharom.langro@gmail.com>
2015-11-11 17:03:15 -0500

    avanze

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
-/* Ingresa tu código Javascript aquí */
+jQuery(function($) {
+
+	//#main-slider
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 8000
+		});
+	});
+
+	$( '.centered' ).each(function( e ) {
+		$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
+	});
+
+	$(window).resize(function(){
+		$( '.centered' ).each(function( e ) {
+			$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
+		});
+	});
+
+
+	$(window).load(function(){'use strict';
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+		var $portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : '.portfolio-item',
+			layoutMode : 'fitRows'
+		});
+		
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
+
+
+$(document).ready(function(){
+  $('.scroll-portafolio a').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("section#portafolio").offset().top
+    }, 1000);
+  }); 
+  $('.scroll-services a').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("section#services").offset().top
+    }, 1000);
+  });
+
+});
